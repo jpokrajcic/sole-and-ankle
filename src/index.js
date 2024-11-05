aa2d77e0c0b4f89a12450994a1107e2434ad9440
Josip Pokrajcic <wastelander2929@gmail.com>
2024-11-05 15:09:59 +0100

    Remove index.css reference

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 import GlobalStyles from './components/GlobalStyles';
-import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
